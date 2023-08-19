49414b5b174555e8da610d13ed0ac43ca4c70590
Dzmitry <gera.1212@yandex.ru>
2023-08-19 21:19:08 +0300

    добавляет header

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,9 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initAccordions} from './vendor/accordion/init-accordion';
+
+import {addToggleMenu} from './modules/header/menu';
 
 // ---------------------------------
 
@@ -21,6 +24,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+
+    addToggleMenu();
+    initAccordions();
   });
 });
 
---
+++ source/js/modules/header/menu.js
@@ -0,0 +1,46 @@
+const addToggleMenu = () => {
+  const OPENED_CLASS = 'is-opened';
+  const LINK_CLASS = 'a';
+  const root = document.querySelector('.header');
+  const toggle = root.querySelector('.header__toggle');
+  const nav = root.querySelector('.nav');
+
+  const onDocumentKeydown = (evt) => {
+    return evt.key === 'Escape' ? closeMenu() : null;
+  };
+
+  const onLinkClick = (evt) => {
+    return (evt.target.matches(LINK_CLASS) && !evt.target.matches('.nav__link--button')) ? closeMenu() : null;
+  };
+
+  const isMenu = (evt) => {
+    return (evt.target.closest('.header') && evt.target.closest('.header__toggle') || evt.target.closest('.nav')) ? evt.preventDefault() : closeMenu();
+  };
+
+  const openMenu = () => {
+    root.classList.add(OPENED_CLASS);
+    toggle.classList.add(OPENED_CLASS);
+    nav.classList.add(OPENED_CLASS);
+    document.addEventListener('keydown', onDocumentKeydown);
+    document.addEventListener('click', isMenu);
+    nav.addEventListener('click', onLinkClick);
+    window.scrollLock.disableScrolling();
+  };
+
+  const closeMenu = () => {
+    root.classList.remove(OPENED_CLASS);
+    toggle.classList.remove(OPENED_CLASS);
+    nav.classList.remove(OPENED_CLASS);
+    document.removeEventListener('keydown', onDocumentKeydown);
+    document.removeEventListener('click', isMenu);
+    nav.removeEventListener('click', onLinkClick);
+    window.scrollLock.enableScrolling();
+  };
+
+  toggle.addEventListener('click', () => {
+    return !toggle.classList.contains(OPENED_CLASS) ? openMenu() : closeMenu();
+  });
+
+};
+
+export {addToggleMenu};
