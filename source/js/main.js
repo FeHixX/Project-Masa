89baff721ee80b1b9d73ae38d64ed479607c8104
Dzmitry <gera.1212@yandex.ru>
2023-08-28 20:08:47 +0300

    добавляет логику кастомного селекта

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initAccordions} from './vendor/accordion/init-accordion';
+import {CustomSelect} from './vendor/select/custom-select';
 
 import {addToggleMenu} from './modules/header/menu';
 import {addHeroSlider} from './modules/hero/slider';
@@ -39,6 +40,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    const select = new CustomSelect();
+    select.init();
   });
 });
 
